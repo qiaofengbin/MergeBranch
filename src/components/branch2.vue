94543c070a9c5fa9aa04a5fc4698f5f6283ef17c
unknown <qiaofengbin0719@163.com>
2019-11-22 15:47:56 +0800

    分支2提交

--- src/components/branch2.vue
+++ src/components/branch2.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="hello">
-    <h1>dev分支代码</h1>
+    <h1>分支2代码</h1>
   </div>
 </template>
 
@@ -16,20 +16,4 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1,
-h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+<style scoped></style>
