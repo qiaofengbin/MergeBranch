af25e016d46a30ccc6c631198d6780d33a7bc4af
unknown <qiaofengbin0719@163.com>
2019-11-25 09:53:36 +0800

    提交list

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,16 @@
 import Vue from "vue";
 import Router from "vue-router";
-import branch2 from "@/components/branch2";
+// import branch2 from "@/components/branch2";
+import list from "../list";
 
 Vue.use(Router);
 
 export default new Router({
   routes: [
     {
-      path: "/branch2",
-      name: "branch2",
-      component: branch2
+      path: "/list",
+      name: "list",
+      component: list
     }
   ]
 });
